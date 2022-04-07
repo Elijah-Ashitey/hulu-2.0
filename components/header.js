19aba7b3f0e8b64c6f22e3a3280d02d604839fbb
Elijah Ashitey <elijahashitey002@gmail.com>
2022-04-07 22:38:19 +0000

    header complete

--- components/header.js
+++ components/header.js
@@ -1,19 +1,45 @@
-import Image from 'next/image';
-import React from 'react';
-
-export default function Header (){
-
-    return(
-
-      <header className="">
-          <h1>This is the header</h1>
-<Image
-src="https://links.papareact.com/ua6"
-width={200} height={100}
-
-/>
-      </header>
-
-    )
-
-}  
+import Image from "next/image";
+import React from "react";
+import HeaderItem from "./headerItem";
+import { LightningBoltIcon, HomeIcon,BadgeCheckIcon,CollectionIcon,SearchIcon,UserIcon } from "@heroicons/react/solid";
+const menus = [
+  {
+    title: "HOME",
+    Icon: HomeIcon,
+  },
+ 
+  {
+    title: "TRENDING",
+    Icon: LightningBoltIcon,
+  },{
+    title: "VERIFIED",
+    Icon: BadgeCheckIcon,
+  },{
+    title: "COLLECTIONS",
+    Icon: CollectionIcon,
+  },{
+    title: "SEARCH",
+    Icon: SearchIcon,
+  },{
+    title: "ACCOUNT",
+    Icon: UserIcon,
+  },
+];
+export default function Header() {
+  return (
+    <header className="flex flex-col sm:flex-row m-5 justify-between items-center h-auto">
+      <div className=" flex flex-grow justify-evenly max-w-2xl">
+        {menus.map((e) => (
+          <HeaderItem title={e.title} Icon={e.Icon} key="" />
+        ))}
+      </div>
+      <Image
+        alt="logo"
+        className="object-contain"
+        src="https://links.papareact.com/ua6"
+        width={200}
+        height={100}
+      />
+    </header>
+  );
+}
